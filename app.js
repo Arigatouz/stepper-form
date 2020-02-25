c40abada40be11cf538ef00c182b75295576db92
Ali Gamal Ali <hictors@gmail.com>
2020-02-26 00:54:04 +0200

    Adding reload button

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const btnNext = document.querySelector(".button-next");
 const btnPrevious = document.querySelector(".button-previous");
 const btnSubmit = document.querySelector(".button-submit");
 const bulletPoint = document.querySelectorAll(".bullet-point");
+const btnReload = document.querySelector(".button-reload");
+
+
 
 let currentVIew = 0;
 
@@ -19,30 +22,38 @@ const tabPreview = tabIndex => {
     btnNext.style.display = "inline-block";
     btnSubmit.style.display = "none";
   }
-  bulletColor(tabIndex);
+  bulletPoint[currentVIew].id = "finish";
 };
 tabPreview(currentVIew);
-
 
 const nextPreview = () => {
   mainTabs[currentVIew].style.display = "none";
   currentVIew++;
   tabPreview(currentVIew);
+  bulletPoint[currentVIew].id = "finish";
 };
 btnNext.addEventListener("click", nextPreview);
-
 
 const previousPreview = () => {
   mainTabs[currentVIew].style.display = "none";
   currentVIew--;
   tabPreview(currentVIew);
+  bulletPoint[currentVIew + 1].id = "";
 };
 btnPrevious.addEventListener("click", previousPreview);
 
-function bulletColor(index) {
-  for (let i = 0; i <= bulletPoint.length; i++) {
-    if (index === i) {
-      bulletPoint[index].id = "finish";
-  }
-}}
-
+const submitted = () => {
+  btnPrevious.style.display = "none";
+  const h1 = document.createElement("h1");
+  h1.textContent = "CONGRATULATIONS";
+  mainTabs[currentVIew].style.display = "none";
+  const message = document.querySelector(".message");
+  message.appendChild(h1);
+  btnSubmit.style.display = "none";
+  btnReload.style.display = 'block'
+  
+};
+btnSubmit.addEventListener("click", submitted);
+btnReload.addEventListener('click' , ()=>{
+  window.location.reload();
+})
